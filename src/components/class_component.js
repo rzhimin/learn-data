9e806e9725722f9db7c7ce2d8a1d5e6b27acaf6a
rizer <254948419@qq.com>
2024-04-09 16:39:49 +0800

    'firstPush'

---
+++ src/components/class_component.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// 自定义一个名为App1的Class组件
+class App1 extends React.Component {
+    render() {
+        return (
+            <h1>这是我的第一个Class组件</h1>
+        )
+    }
+}
+
+// 向外共享这个组件
+export default App1;
