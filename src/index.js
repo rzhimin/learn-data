f4305bb37ddaa15245cefbe956dbb820de50a03e
rizer <254948419@qq.com>
2024-04-11 09:47:26 +0800

    'hebing'

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ import App2 from "./components/function_comp";
 // ReactDOM.render(<App2/>, document.getElementById('root'))
 
 // 引入多组件使用数组
-ReactDOM.render([<App1/>, <App2/>,<h1>test</h1>],document.getElementById('root'))
+ReactDOM.render([<App1 />, <App2 />, <h1>test</h1>], document.getElementById('root'))
